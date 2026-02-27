bc668a20760a39715c62748011faa31d625200e9
evgeny <levenko.evgeny@yandex.by>
2026-02-27 11:08:47 +0300

    fix before k8s

--- src/api/cadet/actionHistoryAPI.js
+++ src/api/cadet/actionHistoryAPI.js
@@ -2,24 +2,24 @@ import BaseAPI from "@/api/baseAPIClass"
 import { axiosInstance as axios } from "@/main.js"
 
 class actionHistoryAPI extends BaseAPI {
-  async getModelsList() {
-    return axios.get(
-      `${import.meta.env.VITE_APP_BACKEND_PROTOCOL}://${import.meta.env.VITE_APP_BACKEND_HOST}:${import.meta.env.VITE_APP_BACKEND_PORT}/api/${this.baseURL}/get_all_models/`,
-    )
-  }
+    async getModelsList() {
+        return axios.get(
+            `${import.meta.env.VITE_APP_BACKEND_PROTOCOL}://${import.meta.env.VITE_APP_BACKEND_HOST}:${import.meta.env.VITE_APP_BACKEND_PORT}/api/${this.baseURL}/get_all_models/`,
+        )
+    }
 }
 
 export default function getActionHistoryAPIInstance() {
-  return new actionHistoryAPI(
-    "action-history",
-    {
-      user: "",
-      action_type: "",
-      record_id: "",
-      model_name: "",
-      date_time_created__gte: "",
-      date_time_created__lte: "",
-    },
-    {},
-  )
+    return new actionHistoryAPI(
+        "action-history",
+        {
+            user: "",
+            action_type: "",
+            record_id: "",
+            model_name: "",
+            date_time_created__gte: "",
+            date_time_created__lte: "",
+        },
+        {},
+    )
 }
